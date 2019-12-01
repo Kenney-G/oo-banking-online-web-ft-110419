2c0add2e680111070af6d127c51a63fecbc59781
Ken Goad <goadke@g.cofc.edu>
2019-12-01 16:16:29 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,7 +1,8 @@
 class Transfer
   attr_accessor :account, :amount
-    def initialize(account, amount)
-      @account = account
-      @amount = amount
-    end
+  
+  def initialize(account, amount)
+    @account = account
+    @amount = amount
+  end
 end
