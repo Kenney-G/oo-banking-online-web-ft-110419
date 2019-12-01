a441920af16e1d3e905651df2cb9386a9d1a94b7
Ken Goad <goadke@g.cofc.edu>
2019-12-01 16:25:01 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,9 +1,10 @@
 class BankAccount
   attr_reader :name, :balance, :status
-      @balance = []
+  
   def initialize(name="Avi", balance = 1000, status = "open")
     @name = name
     @status = status
+    
   end
   
   def deposit(amount)
